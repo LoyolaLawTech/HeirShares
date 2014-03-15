60855d2c126c642032b8b4ca57b7749dcc84bf26
Judson Mitchell <judsonmitchell@gmail.com>
2014-03-15 14:38:48 -0500

    Add handlebars and normalize.css support

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ var deceased = {
 };
 
 
+$('.heir-start').click(function (e) {
+    e.preventDefault();
+    var dName = $('#deceasedName').val();
+    deceased.name = dName;
+    console.log(deceased);
+    var source   = $('#entry-template').html();
+    var template = Handlebars.compile(source);
+    $('.app-content').html(template({deceasedName: dName}));
+});
